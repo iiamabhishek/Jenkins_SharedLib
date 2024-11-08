4a0305c6ac19570b420d74b1ab4e45643de6a4f3
Abhishek Sonwal <59682059+iiamabhishek@users.noreply.github.com>
2024-11-08 15:10:13 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,10 +1,13 @@
 def call(String Project, String ImageTag, String dockerhubuser){
-  withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
-  //withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
-      sh "echo ${dockerhubpass} | docker login --username ${dockerhubuser} --password-stdin"
-      //sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
-      //sh "docker login --username ${dockerhubuser} --password-stdin < ~/docker_pass.txt"
-      //sh "echo $DOCKER_HUB | docker login --username ${dockerhubuser} --password-stdin"
-  }
-  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
+  docker.withRegistry('https://registry-1.docker.io/', 'docker-hub') {
+     echo 'Successfully logged into Docker Hub'
+}
+  // withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
+  // //withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
+  //     sh "echo ${dockerhubpass} | docker login --username ${dockerhubuser} --password-stdin"
+  //     //sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+  //     //sh "docker login --username ${dockerhubuser} --password-stdin < ~/docker_pass.txt"
+  //     //sh "echo $DOCKER_HUB | docker login --username ${dockerhubuser} --password-stdin"
+  // }
+ // sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
