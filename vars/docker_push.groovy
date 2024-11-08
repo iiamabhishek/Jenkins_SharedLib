63c5179c6a3b100e330529f3827856ae366660ac
Abhishek Sonwal <59682059+iiamabhishek@users.noreply.github.com>
2024-11-08 15:38:14 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,5 +1,5 @@
 def call(String Project, String ImageTag, String dockerhubuser){
-  docker.withRegistry('https://registry.example.com', 'credentials-id') {
+  docker.withRegistry('https://registry.example.com', 'docker-hub') {
       echo 'Successfully logged into Docker Hub'
       sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
     }
